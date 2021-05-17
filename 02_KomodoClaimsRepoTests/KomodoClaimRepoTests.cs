4e0ca03d876a8fa522aadab0824929539f22409e
Raymond Sipes <raymondsipes21@gmail.com>
2021-05-17 13:29:05 -0400

    Added claim POCO

---
+++ 02_KomodoClaimsRepoTests/KomodoClaimRepoTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _02_KomodoClaimsRepoTests
+{
+    [TestClass]
+    public class KomodoClaimRepoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 02_KomodoClaimsRepoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("02_KomodoClaimsRepoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_KomodoClaimsRepoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("837e21c0-ee4d-4f06-b293-68bd5ee8322f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
