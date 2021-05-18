f2f378ce7a26d258c9e74f5165afdae25634dc12
Raymond Sipes <raymondsipes21@gmail.com>
2021-05-18 14:40:03 -0400

    Think i did my poco right for badge projects

---
+++ 03_KomodoBadgeRepo/Badge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_KomodoBadgeRepo
+{
+    public class Badge
+    {
+        public int BadgeID { get; set; }
+        public string DoorA1 { get; set; }
+        public string DoorA2 { get; set; }
+        public string DoorA3 { get; set; }
+        public string DoorA4 { get; set; }
+        public string DoorA5 { get; set; }
+
+        public Badge() { }
+        public Badge(int badgeID, string doorA1, string doorA2, string doorA3, string doorA4, string doorA5)
+        {
+            BadgeID = badgeID;
+            DoorA1 = doorA1;
+            DoorA2 = doorA2;
+            DoorA3 = doorA3;
+            DoorA4 = doorA4;
+            DoorA5 = doorA5;
+        }
+    }
+}
---
+++ 03_KomodoBadgeRepoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("03_KomodoBadgeRepoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("03_KomodoBadgeRepoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31e16af8-db7f-4363-95cc-2105fee99dfe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
